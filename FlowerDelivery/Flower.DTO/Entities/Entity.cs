693f882829f1c44e2f50eef79d18d2f43bf7fc39
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-04-16 18:08:54 +0300

    addded basic n-tier

---
+++ FlowerDelivery/Flower.DTO/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerDelivery.DTO.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
